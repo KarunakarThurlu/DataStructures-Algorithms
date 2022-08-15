ea77005c9145410f970401619369d4ff83989d27
Karunakar Thurlu <karunakarthurlujava@gmail.com>
2022-08-15 21:18:44 +0530

    Array Problems

--- src/array/Array.java
+++ src/array/Array.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -67,12 +69,15 @@ public class Array {
 	 * 55. 	Remove Duplicates from Sorted Array
 	 * 56.  Non-decreasing Array
 	 * 57.  Find Duplicate Elements
+	 * 58.  Two Sum II
+	 * 59.  Two Sum less than target
+	 * 60.  Shortest Word Distance
 	 */
 	private static final Logger logger= Logger.getLogger(Array.class.getName());
 	
 	public static void main(String... args) {
 		
-		/*
+		
 		logger.log(Level.INFO, "Two Sum  {0}",Arrays.toString(twoSum(new int[] {1,2,3,4,5,6,7,8,9},12)));  //indices of two elements whose sum is equal to 3  [4,6]
 		logger.log(Level.INFO, "Three Sum  {0}",threeSum(new int[] {-1,0,1,2,-1,-4},0));  //Sum of Three elements in array whose sum is 0 ==> [[-1, -1, 2], [-1, 0, 1], [-1, 0, 1]]
 		logger.log(Level.INFO, "Merge Sorted Arrays  {0}",Arrays.toString(mergeSortedArrays(new int[] {1,4,8,10,19},new int[] {3,5,9,12}))); //Merge Sorted Arrays  [1, 3, 4, 5, 8, 9, 10, 12, 19]
@@ -82,19 +87,29 @@ public class Array {
 		logger.log(Level.INFO, "Sliding Window maximum {0}",Arrays.toString(slidingWindowMaximum(new int[] {1,3,-1,-3,5,3,6,7},3))); //Sliding Window maximum [3, 3, 5, 5, 6, 7]
 		logger.log(Level.INFO, "Combination Sum {0}",combinationSum(new int[] {2,3,5},8)); //Combination Sum [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
 		logger.log(Level.INFO, "Move Zeros to end {0}",Arrays.toString(moveZerosToEnd(new int[] {1,0,2,3,0,4,0,7,8,9}))); //Move Zeros to end [1, 2, 3, 4, 7, 8, 9, 0, 0, 0]
+		logger.log(Level.INFO, "Kth largest Element  {0}",kthLargestElementInArray(new int[] {3,5,1,2,6,9,7,8},4)); //4th largest Element  6
+		logger.log(Level.INFO, "Permutations of Array {0}",permutationsOfGivenArrayElements(new int[] {1,2,3})); // Permutations of Array [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+		logger.log(Level.INFO, "Combinations of Array {0}",combinations(5,2)); // Combinations of Array [[1, 2], [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]]
+		logger.log(Level.INFO, "Merge Interval  {0} :", mergeInterVal(new int[][] {{1,4},{3,6},{7,9},{8,12}})); //Merge Intervals: [{1,6},{7,12},{13,16}]
+		logger.log(Level.INFO, "Merge Interval  {0} :", mergeInterVal(new int[][] {{1,4},{3,6},{7,9},{8,12},new int[]{13,16}})); //[{1,6},{7,12},{13,16}]
+		logger.log(Level.INFO, "Insert Interval {0} :", insertInterVal(new int[][] {{1,3},{6,9}},new int[] {2,5})); //[{1,5},{6,9}}]
+		logger.log(Level.INFO,"Sub array Sum is equal to k  :  {0} ", subArraySumIsEqualToK(new int[] {1,2,3},3)); //Sub array Sum is equal to k  :  2 
+		logger.log(Level.INFO, "Finding Longest Increasing SubArray  {0} ", findingLongestIncreasingSubArray(new int[] {12, 13, 1, 5, 4, 7, 8, 10, 10, 11})); //Finding Longest Increasing SubArray  4 
 		
 		
 		
+		logger.log(Level.INFO, "Two Sum Less Than Target  {0}",twoSumLessThanTarget(new int[] {34,23,1,24,75,33,54,8},60)); //Two Sum Less Tthan Target  58
 		logger.log(Level.INFO, "Left  Rotating Array {0}",Arrays.toString(leftrRotateArray(new int[] {1,2,3,4,5,6,7,8,9},3))); //Left  Rotating Array [4, 5, 6, 7, 8, 9, 1, 2, 3]
 		logger.log(Level.INFO, "Right Rotating Array {0}",Arrays.toString(rightRotateArray(new int[] {1,2,3,4,5,6,7,8,9},3))); //Right Rotating Array [7, 8, 9, 1, 2, 3, 4, 5, 6]
 		logger.log(Level.INFO, "Reversed  Array      {0}",Arrays.toString(reverseArray(new int[] {1,2,3,4,5,6,7,8,9})));      //Reversed  Array  [9, 8, 7, 6, 5, 4, 3, 2, 1]
 		logger.log(Level.INFO, "Reverse Array By Frequency {0}",Arrays.toString(reverseArrayByFrequency(new int[] {1,2,3,4,5,6,7,8,9},3))); //Reversed  Array  by frequency 3  [3, 2, 1, 6, 5, 4, 9, 8, 7]
-		*/
 		
 		
-		logger.log(Level.INFO, "Move Zeros to end {0}",Arrays.toString(moveZerosToEnd(new int[] {1,0,2,3,0,4,0,7,8,9})));
 		
-
+		logger.log(Level.INFO, "Candys required for Distribution  {0} ", candysDistribution(new int[] {1,2,2})); 
+	
+		
+   
 	}
 	
 	/**
@@ -113,6 +128,48 @@ public class Array {
 		return new int[] {-1,-1};
 	}
 	
+	/**
+	 * Two Sum II (Array Elements are already sorted
+	 * @param array
+	 * @param target
+	 * @return
+	 */
+	public static int[] twoSumII(int[] array,int target) {
+		int l=0;
+		int r=array.length-1;
+		while(l<=r) {
+			int s=array[l]+array[r];
+			if(s==target)
+				return new int[] {l+1,r+1};
+			else if(s<target)
+				l++;
+			else 
+				r--;
+		}
+		return new int[] {-1,-1};
+	}
+	/**
+	 * Two Sum less Than target
+	 * @param array
+	 * @param target
+	 * @return 
+	 */
+	public static int twoSumLessThanTarget(int[] array,int target) {
+		Arrays.sort(array);
+		int max=Integer.MIN_VALUE;
+		int l=0;
+		int r=array.length-1;
+		while(l<r) {
+			int s=array[l]+array[r];
+			if(s<=target) {
+				max=Math.max(max, s);
+				l++;
+			}else {
+				r--;
+			}
+		}
+		return max;
+	}
 	/**
 	 * 
 	 * @param array
@@ -294,6 +351,222 @@ public class Array {
 		}
 		return array;
 	}
+	
+	/**
+	 * 
+	 * @param array
+	 * @param k
+	 * @return
+	 */
+	public static int kthLargestElementInArray(int[] array,int k) {
+		Queue<Integer> queue=new PriorityQueue<>();
+		for(int i=0;i<array.length;i++) {
+			queue.offer(array[i]);
+			if(queue.size()>k) {
+				queue.remove();
+			}
+		}
+		return queue.peek();
+	}
+
+	/**
+	 * 
+	 * @param a
+	 * @return
+	 */
+	public static List<List<Integer>> permutationsOfGivenArrayElements(int[] a) {
+		List<List<Integer>> res=new ArrayList<>();
+		List<Integer> list=new ArrayList<>();
+		boolean[] visited=new boolean[a.length];
+		permutationsOfGivenArrayElements(res,list,a,visited);
+		return res;
+	}
+	
+	/**
+	 * 
+	 * @param res
+	 * @param list
+	 * @param a
+	 * @param i
+	 */
+	private static void permutationsOfGivenArrayElements(List<List<Integer>> res, List<Integer> list, int[] a,boolean[] visited) {
+		if(list.size()==a.length) {
+			res.add(new ArrayList<>(list));
+			return;
+		}
+		for(int i=0;i<a.length;i++) {
+			if(!visited[i]) {
+				visited[i]=true;
+				list.add(a[i]);
+				permutationsOfGivenArrayElements(res,list,a,visited);
+				visited[i]=false;
+				list.remove(list.size()-1);
+			}
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @param k
+	 * @return
+	 */
+	public static List<List<Integer>> combinations(int n,int k){
+		List<List<Integer>> res=new ArrayList<>();
+		List<Integer> l=new ArrayList<>();
+		combinations(res,l,n,k,1);
+		return res;
+	}
+	
+	/**
+	 * Merge Intervals of given array
+	 * @param array
+	 * @return
+	 */
+	public static String mergeInterVal(int[][] array){
+		Arrays.sort(array, (a,b)->a[0]-b[0]);
+		List<int[]> res=new ArrayList<>();
+		int[] prev=array[0];
+		for(int[] curr:array) {
+			int cs=curr[0];
+			int ce=curr[1];
+			int pe=prev[1];
+			if(cs<=pe) {
+				prev[1]=Math.max(ce, pe);
+			}else {
+				res.add(prev);	
+				prev=curr;
+			}
+		}
+		res.add(prev);
+		for(int i=0;i<res.size();i++)
+			logger.log(Level.INFO, "MergeInterval of Array {0}", Arrays.toString(res.get(i)));
+		return "Merge Intervals: ";
+	}
+	
+	/**
+	 * { {1,4},{5,6},{9,10}} {7,8}
+	 * @param array
+	 * @param interval
+	 * @return
+	 */
+	public static String insertInterVal(int[][] array,int[] interval) {
+		Arrays.sort(array,(a,b)->a[0]-b[0]);
+		List<int[]> list=new ArrayList<>();
+		int[] temp=interval;
+		for(int i=0;i<array.length;i++) {
+			if(temp[0]>array[i][1]) {
+				list.add(array[i]);
+			}else if(temp[1]<array[i][1]) {
+				list.add(temp);
+				temp=array[i];
+			}else {
+				temp[0]=Math.min(temp[0], array[i][0]);
+				temp[1]=Math.max(temp[1], array[i][1]);
+			}
+		}
+		list.add(temp);
+		for(int i=0;i<list.size();i++)
+			logger.log(Level.INFO, "MergeInterval of Array {0}", Arrays.toString(list.get(i)));
+		return "Insert Interval: ";
+	}
+	
+	/**
+	 * 
+	 * @param array
+	 * @param k
+	 * @return
+	 */
+	public static int subArraySumIsEqualToK(int[] array,int k) {
+		int n=array.length;
+		int sum=0;
+		int count=0;
+		for(int i=0;i<n;i++) {
+			sum=array[i];
+			if(sum==k)
+				count++;
+			for(int j=i+1;j<n;j++) {
+				sum=sum+array[j];
+				if(sum==k)
+					count++;
+			}
+		}
+		return count;		
+	}
+	/**
+	 * Find peak element
+	 * @param array
+	 * @return
+	 */
+	public static int findPeakElement(int[] array) {
+		for(int i=0;i<array.length-1;i++) {
+			if(array[i]>array[i+1] ){
+				return i;
+			}
+		}
+		return array.length-1;
+	}
+	/**
+	 * 
+	 * @param array
+	 * @return
+	 */
+	public static int findingLongestIncreasingSubArray(int[] array) {
+		int count=1;
+		int max=0;
+		for(int i=0;i<array.length-1;i++) {
+			if(array[i+1]>array[i]) {
+				count++;
+			}else {
+				max=Math.max(max, count);
+				count=1;
+			}
+		}
+		return max;
+	}
+	
+	/**
+	 * 
+	 * @param ratings
+	 * @return
+	 */
+	public static int candysDistribution(int[] ratings) {
+		int n=ratings.length;
+		int[] cost=new int[n];
+		Arrays.fill(cost, 1);
+		for(int i=1;i<n;i++) {
+			if(ratings[i]>ratings[i-1])
+				cost[i]=cost[i-1]+1;
+		}
+		int s=cost[cost.length-1];
+		for(int i=ratings.length-2;i>=0;i--) {
+			if(ratings[i]>ratings[i+1])
+				cost[i]=Math.max(cost[i], cost[i+1]+1);
+			s=s+cost[i];
+		}
+		return s;
+	}
+	/**
+	 * 
+	 * @param res
+	 * @param l
+	 * @param n
+	 * @param k
+	 * @param index
+	 */
+	private static void combinations(List<List<Integer>> res, List<Integer> l, int n, int k, int index) {
+		if(k==0) {
+			res.add(new ArrayList<>(l));
+			return;
+		}
+		for(int i=index;i<=n-k+1;i++) {
+			l.add(i);
+			combinations(res,l,n,k-1,i+1);
+			l.remove(l.size()-1);
+		}
+	}
+
 	/**
 	 * 
 	 * @param n
@@ -414,7 +687,30 @@ public class Array {
 		return Boolean.TRUE;
 	}
 	
-	
+	/**
+	 * 
+	 * @param words
+	 * @param word1
+	 * @param word2
+	 * @return
+	 */
+	public static int shortestWordDistance(String[] words,String word1,String word2) {
+		int i1=-1;
+		int i2=-1;
+		int max=words.length;
+		for(int i=0;i<words.length;i++) {
+			if(words[i].equals(word1)) {
+				i1=i;
+			}
+			if(words[i].equals(word2)) {
+				i2=i;
+			}
+			if(i1!=-1 && i2!=-1) {
+				max=Math.min(max, Math.abs(i1-i2));
+			}
+		}
+		return max;
+	}
 	
 	
 	/**
