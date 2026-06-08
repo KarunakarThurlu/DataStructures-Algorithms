74dface9f8b1c7fe744f759b391f6e8ddb240b3e
KarunakarThurlu <karunakarthurlujava@gmail.com>
2026-06-08 20:39:29 +0530

    KarunakarThurlu | DesignPatterns

---
+++ src/main/java/designpatterns/DesignPatternsDefinations.java
@@ -0,0 +1,54 @@
+package designpatterns;
+
+public class DesignPatternsDefinations {
+/*
+    
+
+========================
+Design Patterns Types : 
+========================
+
+1. Creational Patterns : Talks about object creation.
+-----------------------
+
+1. Singleton : Singleton ensures that only one instance of a class exists and provides a global access point to it.
+2. Factory Method
+3. Abstract Factory
+4. Builder
+5. Prototype
+
+Key Point : Creational patterns deal with object creation mechanisms.
+
+
+2. Structural Patterns: Talks Class/Object composition
+----------------------- 
+
+1. Adapter
+2. Decorator
+3. Proxy
+4. Facade
+5. Composite
+6. Bridge
+7. Flyweight
+
+Interview Line: Structural patterns define how classes and objects are combined to form larger structures.
+
+3. Behavioral Patterns : Talks about Communication and behavior
+------------------------
+1. Strategy
+2. Observer
+3. Template Method
+4. Command
+5. State
+6. Chain of Responsibility
+7. Mediator
+8. Iterator
+9. Visitor
+10.Memento
+11.Interpreter
+
+Key point : Behavioral patterns focus on interaction and responsibility between objects
+ 
+ 
+ */
+}
---
+++ src/main/java/designpatterns/StrategyPattern.java
@@ -0,0 +1,5 @@
+package designpatterns;
+
+public class StrategyPattern {
+
+}
